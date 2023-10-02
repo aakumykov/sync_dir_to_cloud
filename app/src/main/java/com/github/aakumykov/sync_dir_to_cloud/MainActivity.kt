81c6b15f56a90f416c83c9c6bd8202d0f2060dc9
Андрей Кумыков <aakumykov@yandex.ru>
2023-10-02 12:32:20 +0300

    Заготовка.

---
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.aakumykov.sync_dir_to_cloud
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
