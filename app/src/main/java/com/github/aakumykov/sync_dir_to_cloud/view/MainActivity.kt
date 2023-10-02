48b024b295ab7e88679e3a7505ebafd276d00eb0
Андрей Кумыков <aakumykov@yandex.ru>
2023-10-02 13:37:55 +0300

    *

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.github.aakumykov.sync_dir_to_cloud.R
 import com.github.aakumykov.sync_dir_to_cloud.databinding.ActivityMainBinding
 import com.github.aakumykov.sync_dir_to_cloud.view.fragments.TaskEditFragment
 import com.github.aakumykov.sync_dir_to_cloud.view.fragments.TaskListFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private val DEFAULT_BACK_STACK_NAME = "default_back_stack"
     private lateinit var binding: ActivityMainBinding
     private lateinit var navigationViewModel: NavigationViewModel
     private lateinit var fragmentManager: androidx.fragment.app.FragmentManager
@@ -28,13 +30,28 @@ class MainActivity : AppCompatActivity() {
 
     private fun onNewNavTarget(navTarget: NavTarget) {
         when (navTarget) {
-            NavStart, NavList -> setFragment(TaskListFragment.create())
-            NavAdd -> loadFragment(TaskEditFragment.create())
-            NavEdit -> loadFragment(TaskEditFragment.create((NavEdit) navTarget.id))
+//            is NavStart, NavList -> setFragment(TaskListFragment.create())
+            is NavAdd -> loadFragment(TaskEditFragment.create())
+            is NavEdit -> loadFragment(TaskEditFragment.create(navTarget.id))
+            else -> setFragment(TaskListFragment.create())
         }
     }
 
     private fun setFragment(fragment: Fragment) {
 
+        fragmentManager.clearBackStack(DEFAULT_BACK_STACK_NAME)
+
+        fragmentManager.beginTransaction()
+            .setReorderingAllowed(false)
+            .replace(R.id.fragmentContainerView, fragment, null)
+            .commitNow()
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        fragmentManager.beginTransaction()
+            .addToBackStack(DEFAULT_BACK_STACK_NAME)
+            .setReorderingAllowed(true)
+            .replace(R.id.fragmentContainerView, fragment)
+            .commit()
     }
 }
--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/NavTarget.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/NavTarget.kt
@@ -3,9 +3,6 @@ package com.github.aakumykov.sync_dir_to_cloud.view
 sealed class NavTarget
 
 object NavStart : NavTarget()
-
 object NavList : NavTarget()
-
 object NavAdd : NavTarget()
-
 class NavEdit(val id: String?) : NavTarget()
