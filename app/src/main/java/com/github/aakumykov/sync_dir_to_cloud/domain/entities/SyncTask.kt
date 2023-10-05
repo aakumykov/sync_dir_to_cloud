6122e41626321503d26125f5606a7cbbf07b4fb5
Андрей Кумыков <aakumykov@yandex.ru>
2023-10-05 15:36:06 +0300

    Добавил поле isProgress в SyncTask.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/domain/entities/SyncTask.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/domain/entities/SyncTask.kt
@@ -1,5 +1,6 @@
 package com.github.aakumykov.sync_dir_to_cloud.domain.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Ignore
 import androidx.room.PrimaryKey
@@ -10,18 +11,31 @@ class SyncTask : SyncTaskBase {
 
     constructor(syncTaskBase: SyncTaskBase) : super(syncTaskBase.sourcePath, syncTaskBase.targetPath) {
         id = UUID.randomUUID().toString()
+        isProgress = false
     }
 
-    constructor(id: String, sourcePath: String, targetPath: String) : super(sourcePath, targetPath) {
+    constructor(id: String,
+                sourcePath: String,
+                targetPath: String,
+                isProgress: Boolean
+    ) : super(sourcePath, targetPath) {
         this.id = id
+        this.isProgress = isProgress
     }
 
     @PrimaryKey val id: String
 
-    @Ignore
-    fun getTitle() = "$sourcePath - $targetPath"
+    @Ignore fun getTitle() = "$sourcePath - $targetPath"
+
+    @ColumnInfo(name = "is_progress") val isProgress: Boolean
+
 
     fun updateValues(syncTaskBase: SyncTaskBase): SyncTask {
-        return SyncTask(id, syncTaskBase.sourcePath, syncTaskBase.targetPath)
+        return SyncTask(
+            id,
+            syncTaskBase.sourcePath,
+            syncTaskBase.targetPath,
+            isProgress
+        )
     }
 }
--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/repository/room/AppDatabase.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/repository/room/AppDatabase.kt
@@ -7,7 +7,7 @@ import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncTask
 
 @Database(
     entities = [ SyncTask::class ],
-    version = 3,
+    version = 4,
     autoMigrations = []
 )
 abstract class AppDatabase : RoomDatabase() {
