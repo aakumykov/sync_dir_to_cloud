e0ce0af266fe27e829120274166cb44c4fef5d0c
Андрей Кумыков <aakumykov@yandex.ru>
2023-10-12 15:13:41 +0300

    Делаю CloudAuthListFragment, CloudAuthListViewModel и думаю...

---
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/cloud_auth_list/CloudAuthListViewModel.kt
@@ -0,0 +1,6 @@
+package com.github.aakumykov.sync_dir_to_cloud.view.cloud_auth_list
+
+import android.app.Application
+import com.github.aakumykov.sync_dir_to_cloud.view.common_view_models.PageTitleViewModel
+
+class CloudAuthListViewModel(application: Application) : PageTitleViewModel(application)
