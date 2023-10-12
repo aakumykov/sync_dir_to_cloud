a9a0c4222a3d624164dff367719b59edc513c5bd
Андрей Кумыков <aakumykov@yandex.ru>
2023-10-12 15:10:50 +0300

    Сделал PageTitleViewModel абстрактной.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/common_view_models/PageTitleViewModel.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/common_view_models/PageTitleViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class PageTitleViewModel : ViewModel() {
+abstract class PageTitleViewModel : ViewModel() {
 
     private val pageTitleLiveData = MutableLiveData<String>()
 
