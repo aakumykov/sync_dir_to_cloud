d30dd883c76288e5f421c71393870c564ddf612a
Андрей Кумыков <aakumykov@yandex.ru>
2023-10-25 21:16:45 +0300

    Revert "Начал делать получение CloudAuthSetter через Dagger, и получается чересчур сложно и запутано - признак кривой реализации."
    This reverts commit 53f446daa918c214ae85fc1e14dd82a755aa19b2.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/cloud_auth_list/AuthListFragment.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/cloud_auth_list/AuthListFragment.kt
@@ -3,15 +3,14 @@ package com.github.aakumykov.sync_dir_to_cloud.view.cloud_auth_list
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import com.github.aakumykov.sync_dir_to_cloud.App
 import com.github.aakumykov.sync_dir_to_cloud.R
 import com.github.aakumykov.sync_dir_to_cloud.databinding.FragmentAuthListBinding
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.CloudAuth
 import com.github.aakumykov.sync_dir_to_cloud.view.cloud_auth_edit.AuthEditFragment
-import com.github.aakumykov.sync_dir_to_cloud.view.task_edit.CloudAuthSetter
 import com.github.aakumykov.sync_dir_to_cloud.view.utils.ListViewAdapter
 import kotlinx.coroutines.launch
 
@@ -21,9 +20,8 @@ class AuthListFragment : DialogFragment(R.layout.fragment_auth_list) {
     private val binding get() = _binding!!
 
     private lateinit var listAdapter: ListViewAdapter<CloudAuth>
-    private val viewModel: AuthListViewModel by viewModels()
 
-    private lateinit var cloudAuthSetter: CloudAuthSetter
+    private val viewModel: AuthListViewModel by viewModels()
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -31,9 +29,7 @@ class AuthListFragment : DialogFragment(R.layout.fragment_auth_list) {
         prepareLayout(view)
         prepareButtons()
         prepareViewModel()
-        prepareListView()
-
-        cloudAuthSetter = App.getAppComponent().getCloudAuthSetter()
+        prepareList()
     }
 
 
@@ -68,7 +64,7 @@ class AuthListFragment : DialogFragment(R.layout.fragment_auth_list) {
     }
 
 
-    private fun prepareListView() {
+    private fun prepareList() {
 
         listAdapter = ListViewAdapter<CloudAuth>(
             requireContext(),
@@ -81,7 +77,7 @@ class AuthListFragment : DialogFragment(R.layout.fragment_auth_list) {
 
         binding.listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
             listAdapter.getItem(position)?.let {
-
+                Toast.makeText(requireContext(), it.name, Toast.LENGTH_SHORT).show()
                 dismiss()
             }
         }
