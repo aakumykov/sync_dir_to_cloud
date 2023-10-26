9e7b382df7b2ae33bc1bd831b7e2fbeab600c272
Андрей Кумыков <aakumykov@yandex.ru>
2023-10-26 22:25:58 +0300

    Добавил в объект CloudAuth индекс, требующий уникальности поля name.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/domain/entities/CloudAuth.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/domain/entities/CloudAuth.kt
@@ -3,13 +3,14 @@ package com.github.aakumykov.sync_dir_to_cloud.domain.entities
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Ignore
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import java.util.UUID
 
 @Entity(
-    tableName = "cloud_auth"
+    tableName = "cloud_auth",
+    indices = [ Index(value = ["name"], unique = true) ]
 )
-// FIXME: уникальность имён
 class CloudAuth (
     @PrimaryKey val id: String,
     val name: String,
