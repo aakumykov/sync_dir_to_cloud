ed942fbbdd49eacd43c21ba34c70722ffe535d92
Андрей Кумыков <aakumykov@yandex.ru>
2023-11-03 14:57:56 +0300

    Revert "Начал "минимальной кровью" превращать SyncTask в объект-контейнер."
    This reverts commit 330eba1b41fc520085b34519b4924ca65a4dbbd5.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/domain/entities/SyncTask.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/domain/entities/SyncTask.kt
@@ -1,12 +1,64 @@
 package com.github.aakumykov.sync_dir_to_cloud.domain.entities
 
-import androidx.room.Embedded
-import androidx.room.Relation
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+import java.util.UUID
 
-class SyncTask (
-    @Embedded
-    val syncTaskBase: SyncTaskBase,
+@Entity(tableName = "sync_tasks")
+class SyncTask {
 
-    @Relation(entity = CloudAuth::class, parentColumn = "auth_id", entityColumn = "id")
-    val cloudAuth: CloudAuth
-)
+    @PrimaryKey var id: String = UUID.randomUUID().toString()
+    @ColumnInfo(name = "state") var state: State = State.IDLE
+    @ColumnInfo(name = "is_enabled") var isEnabled: Boolean = false
+
+    @ColumnInfo(name = "source_path") var sourcePath: String?
+    @ColumnInfo(name = "target_path") var targetPath: String?
+    @ColumnInfo(name = "interval_h") var intervalHours: Int
+    @ColumnInfo(name = "interval_m") var intervalMinutes: Int
+
+    @ColumnInfo(name = "cloud_auth_id") var cloudAuthId: String? = null
+
+    @Ignore
+    constructor() {
+        this.sourcePath = null
+        this.targetPath = null
+        this.intervalHours = 0
+        this.intervalMinutes = 0
+    }
+
+    constructor(sourcePath: String, targetPath: String, intervalHours: Int, intervalMinutes: Int) : this() {
+        this.sourcePath = sourcePath
+        this.targetPath = targetPath
+        this.intervalHours = intervalHours
+        this.intervalMinutes = intervalMinutes
+    }
+
+
+    fun getTitle(): String {
+        return "$sourcePath -> $targetPath"
+    }
+
+
+    @Ignore
+    fun getExecutionIntervalMinutes(): Long {
+        return (intervalHours * 60 + intervalMinutes) * 60L
+    }
+
+
+    override fun toString(): String {
+        return SyncTask::class.simpleName + " { enabled: " + isEnabled + ", " + sourcePath + " -> " + targetPath + " }"
+    }
+
+    enum class State {
+        IDLE,
+        RUNNING,
+        SUCCESS,
+        ERROR,
+    }
+
+    companion object {
+        val TAG = SyncTask::class.simpleName.toString()
+    }
+}
