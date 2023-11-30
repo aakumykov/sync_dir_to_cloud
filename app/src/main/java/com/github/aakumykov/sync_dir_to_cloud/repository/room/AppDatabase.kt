47415fc2bdddb586880fe431eb09ae91dbaca9f1
Andrey Kumykov <aakumykov@yandex.ru>
2023-11-30 14:58:46 +0300

    Убрал из SyncObject поле is_listed.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/repository/room/AppDatabase.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/repository/room/AppDatabase.kt
@@ -9,7 +9,7 @@ import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncTask
 
 @Database(
     entities = [ SyncTask::class, SyncObject::class, CloudAuth::class ],
-    version = 13,
+    version = 14,
     autoMigrations = []
 )
 abstract class AppDatabase : RoomDatabase() {
