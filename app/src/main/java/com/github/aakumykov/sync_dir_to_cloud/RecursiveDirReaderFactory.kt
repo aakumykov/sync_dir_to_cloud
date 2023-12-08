b92bf19bed687374105ab78b8484ab62badf72df
Andrey Kumykov <aakumykov@yandex.ru>
2023-12-08 14:18:44 +0300

    Подшаманил RecursiveDirReaderFactory.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/RecursiveDirReaderFactory.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/RecursiveDirReaderFactory.kt
@@ -3,15 +3,13 @@ package com.github.aakumykov.sync_dir_to_cloud
 import com.github.aakumykov.recursive_dir_reader.RecursiveDirReader
 import com.github.aakumykov.sync_dir_to_cloud.di.file_lister.assisted_factories.FileListerAssistedFactory
 import javax.inject.Inject
-import kotlin.jvm.Throws
 
 class RecursiveDirReaderFactory @Inject constructor(
     private val fileListerAssistedFactoriesMap: Map<StorageType, @JvmSuppressWildcards FileListerAssistedFactory>
 ) {
-
-    // TODO: убрать null-able, добавить исключение...
-//    @Throws
     fun create(storageType: StorageType, authToken: String): RecursiveDirReader? {
-        return null
+        return fileListerAssistedFactoriesMap[storageType]?.let { factory ->
+            RecursiveDirReader(factory.create(authToken))
+        }
     }
 }
