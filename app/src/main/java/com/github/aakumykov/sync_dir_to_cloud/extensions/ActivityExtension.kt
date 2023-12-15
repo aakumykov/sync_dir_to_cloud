f630e1b47b0120a4af1315caddd3bfaa01eb8f84
Andrey Kumykov <aakumykov@yandex.ru>
2023-12-15 16:11:30 +0300

    Расширение класса Activity с методом открытия страницы свойств приложения.

---
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/extensions/ActivityExtension.kt
@@ -0,0 +1,12 @@
+package com.github.aakumykov.sync_dir_to_cloud.extensions
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+
+fun Activity.openAppProperties() {
+    val uri = Uri.parse("package:$packageName")
+    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, uri)
+    if (intent.resolveActivity(packageManager) != null) { startActivity(intent) }
+}
