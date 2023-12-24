2540e85bb8c7885166f3eb05741995282dd69755
Andrey Kumykov <aakumykov@yandex.ru>
2023-12-25 01:59:07 +0300

    Метод SyncObjectDAO.add() перезаписывает элемент при совпадении (конфликте).

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/repository/room/SyncObjectDAO.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/repository/room/SyncObjectDAO.kt
@@ -2,11 +2,12 @@ package com.github.aakumykov.sync_dir_to_cloud.repository.room
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncObject
 
 @Dao
 interface SyncObjectDAO {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun add(syncObject: SyncObject)
 }
