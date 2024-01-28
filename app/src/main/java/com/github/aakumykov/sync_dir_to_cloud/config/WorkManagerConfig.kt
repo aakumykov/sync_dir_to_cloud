57e624a78c1a1e4c4f8465375c368d7ae11d2e57
Andrey Kumykov <aakumykov@yandex.ru>
2024-01-28 07:07:58 +0300

    Настройки гибкого интервала периодической работы в WorkManagerConfig.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/config/WorkManagerConfig.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/config/WorkManagerConfig.kt
@@ -1,7 +1,13 @@
 package com.github.aakumykov.sync_dir_to_cloud.config
 
+import androidx.work.PeriodicWorkRequest
+import java.util.concurrent.TimeUnit
+
 class WorkManagerConfig private constructor() {
     companion object {
+        const val PERIODIC_FLEX_INTERVAL: Long = PeriodicWorkRequest.MIN_PERIODIC_FLEX_MILLIS
+        val PERIODIC_FLEX_UNITS: TimeUnit = TimeUnit.MILLISECONDS
+
         const val PERIODIC_WORK_ID_PREFIX = "PERIODIC-"
         const val MANUAL_WORK_ID_PREFIX = "MANUAL-"
         const val PROBE_WORK_ID_PREFIX = "PROBE-"
