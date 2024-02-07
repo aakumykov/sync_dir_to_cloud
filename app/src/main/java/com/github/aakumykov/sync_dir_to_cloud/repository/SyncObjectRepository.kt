84316cedb4378baeb27241db4cfa7357d62ab73a
Andrey Kumykov <aakumykov@yandex.ru>
2024-02-07 09:54:21 +0300

    Избавился от SyncObjectLocalDataSource.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/repository/SyncObjectRepository.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/repository/SyncObjectRepository.kt
@@ -3,47 +3,50 @@ package com.github.aakumykov.sync_dir_to_cloud.repository
 import androidx.lifecycle.LiveData
 import com.github.aakumykov.sync_dir_to_cloud.di.annotations.AppScope
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.ExecutionState
+import com.github.aakumykov.sync_dir_to_cloud.domain.entities.ModificationState
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncObject
 import com.github.aakumykov.sync_dir_to_cloud.interfaces.for_repository.sync_object.SyncObjectAdder
-import com.github.aakumykov.sync_dir_to_cloud.interfaces.for_repository.sync_object.SyncObjectStateResetter
 import com.github.aakumykov.sync_dir_to_cloud.interfaces.for_repository.sync_object.SyncObjectReader
 import com.github.aakumykov.sync_dir_to_cloud.interfaces.for_repository.sync_object.SyncObjectStateChanger
-import com.github.aakumykov.sync_dir_to_cloud.repository.data_sources.SyncObjectLocalDataSource
+import com.github.aakumykov.sync_dir_to_cloud.interfaces.for_repository.sync_object.SyncObjectStateResetter
+import com.github.aakumykov.sync_dir_to_cloud.repository.room.SyncObjectDAO
 import javax.inject.Inject
 
-// TODO: LocalDataSource --> DataSource
 // TODO: передавать диспетчер сопрограммы
-// TODO: избавиться от LocalDataSource
 @AppScope
-class SyncObjectRepository @Inject constructor(private val syncObjectLocalDataSource: SyncObjectLocalDataSource)
+class SyncObjectRepository @Inject constructor(
+    private val syncObjectDAO: SyncObjectDAO
+)
     : SyncObjectAdder, SyncObjectReader, SyncObjectStateChanger, SyncObjectStateResetter
 {
     override suspend fun addSyncObject(syncObject: SyncObject) {
-        syncObjectLocalDataSource.addSyncObject(syncObject)
+        syncObjectDAO.add(syncObject)
     }
 
+    // TODO: удалить
     override suspend fun getSyncObjectsForTask(taskId: String): List<SyncObject>
-        = syncObjectLocalDataSource.getSyncObjectsForTask(taskId)
+        = syncObjectDAO.getSyncObjectsForTask(taskId)
 
+    // TODO: переименовать в getSyncObjectsForTaskWithModificationState()
     override suspend fun getNewAndChangedSyncObjectsForTask(taskId: String): List<SyncObject>
-        = syncObjectLocalDataSource.getNewAndChangedSyncObjectsForTask(taskId)
+        = syncObjectDAO.getNewAndChangedSyncObjectsForTask(taskId, arrayOf(ModificationState.NEW, ModificationState.MODIFIED))
 
+    // TODO: state --> executionState
     override suspend fun changeExecutionState(syncObjectId: String, state: ExecutionState, errorMsg: String) {
-        syncObjectLocalDataSource.setState(syncObjectId, state, errorMsg)
+        syncObjectDAO.setExecutionState(syncObjectId, state, errorMsg)
     }
 
     override suspend fun getSyncObjectListAsLiveData(taskId: String): LiveData<List<SyncObject>>
-        = syncObjectLocalDataSource.getSyncObjectList(taskId)
+        = syncObjectDAO.getSyncObjectList(taskId)
 
     override suspend fun setSyncDate(id: String, date: Long)
-        = syncObjectLocalDataSource.setSyncDate(id, date)
+        = syncObjectDAO.setSyncDate(id, date)
 
     override suspend fun resetSyncObjectsStateOfTask(taskId: String) {
-//        syncObjectLocalDataSource.deleteSyncObjectOfTask(taskId)
-        syncObjectLocalDataSource.markAllItemsAsDeleted(taskId)
+        syncObjectDAO.setStateOfAllItems(taskId, ModificationState.DELETED)
     }
 
     override suspend fun getSyncObject(name: String, relativeParentDirPath: String): SyncObject? {
-        return syncObjectLocalDataSource.getSyncObject(name, relativeParentDirPath)
+        return syncObjectDAO.getSyncObject(name, relativeParentDirPath)
     }
 }
