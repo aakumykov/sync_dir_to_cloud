5878af43b0c9def526bb2d26b4f9a44f4f58cf04
Andrey Kumykov <aakumykov@yandex.ru>
2024-04-28 11:27:30 +0300

    Компилятору перестал нравиться конструктор SyncTask...

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/enums/SortingMode.kt
+++
@@ -1,6 +0,0 @@
-package com.github.aakumykov.sync_dir_to_cloud.enums
-
-enum class SortingMode {
-    BY_NAME_DIRECT,
-    BY_NAME_REVERSE
-}
---
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/file_selector_factory/FileSelectorFactory.kt
@@ -0,0 +1,28 @@
+package com.github.aakumykov.sync_dir_to_cloud.file_selector_factory
+
+import com.github.aakumykov.file_lister_navigator_selector.file_lister.SimpleSortingMode
+import com.github.aakumykov.file_lister_navigator_selector.file_selector.FileSelectorFragment
+import com.github.aakumykov.file_lister_navigator_selector.local_file_selector.LocalFileSelectorFragment
+import com.github.aakumykov.sync_dir_to_cloud.domain.entities.CloudAuth
+import com.github.aakumykov.sync_dir_to_cloud.enums.StorageType
+import com.github.aakumykov.yandex_disk_file_lister_navigator_selector.yandex_disk_file_selector.YandexDiskFileSelectorFragment
+
+class FileSelectorFactory {
+
+    fun create(
+        fragmentResultKey: String,
+        sourceStorageType: StorageType,
+        cloudAuth: CloudAuth
+    )
+        : FileSelectorFragment<SimpleSortingMode>
+    {
+        return when(sourceStorageType) {
+            StorageType.LOCAL -> LocalFileSelectorFragment.create(fragmentResultKey)
+
+            StorageType.YANDEX_DISK -> YandexDiskFileSelectorFragment.create(
+                fragmentResultKey,
+                cloudAuth.authToken
+            )
+        }
+    }
+}
