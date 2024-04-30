1289f6fafce69ddfb4bc4f62b99a08995ae3113c
Andrey Kumykov <aakumykov@yandex.ru>
2024-04-30 07:53:01 +0300

    Вынес общую логику SourceReader-ов в базовый класс.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/di/modules/SourceReaderAssistedFactoriesModule.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/di/modules/SourceReaderAssistedFactoriesModule.kt
@@ -2,8 +2,8 @@ package com.github.aakumykov.sync_dir_to_cloud.di.modules
 
 import com.github.aakumykov.sync_dir_to_cloud.di.annotations.KeyStorageType
 import com.github.aakumykov.sync_dir_to_cloud.enums.StorageType
-import com.github.aakumykov.sync_dir_to_cloud.sync_task_executor.source_reader.LocalSourceReader
-import com.github.aakumykov.sync_dir_to_cloud.sync_task_executor.source_reader.YandexSourceReader
+import com.github.aakumykov.sync_dir_to_cloud.sync_task_executor.source_reader.local_source_reader.LocalSourceReaderAssistedFactory
+import com.github.aakumykov.sync_dir_to_cloud.sync_task_executor.source_reader.yandex_source_reader.YandexSourceReaderAssistedFactory
 import com.github.aakumykov.sync_dir_to_cloud.sync_task_executor.source_reader.interfaces.SourceReaderAssistedFactory
 import dagger.Binds
 import dagger.Module
@@ -15,10 +15,10 @@ interface SourceReaderAssistedFactoriesModule {
     @Binds
     @IntoMap
     @KeyStorageType(StorageType.LOCAL)
-    fun bindLocalSourceReader(factory: LocalSourceReader.Factory): SourceReaderAssistedFactory
+    fun bindLocalSourceReader(factory: LocalSourceReaderAssistedFactory): SourceReaderAssistedFactory
     
     @Binds
     @IntoMap
     @KeyStorageType(StorageType.YANDEX_DISK)
-    fun bindYandexSourceReader(factory: YandexSourceReader.Factory): SourceReaderAssistedFactory
+    fun bindYandexSourceReader(factory: YandexSourceReaderAssistedFactory): SourceReaderAssistedFactory
 }
