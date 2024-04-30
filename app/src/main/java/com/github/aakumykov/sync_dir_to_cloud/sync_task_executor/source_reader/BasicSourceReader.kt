12806e5537fc7b342362479ff955f8c84c2a2525
Andrey Kumykov <aakumykov@yandex.ru>
2024-04-30 07:53:01 +0300

    Вынес общую логику SourceReader-ов в базовый класс.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/sync_task_executor/source_reader/BasicSourceReader.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/sync_task_executor/source_reader/BasicSourceReader.kt
@@ -3,7 +3,6 @@ package com.github.aakumykov.sync_dir_to_cloud.sync_task_executor.source_reader
 import com.github.aakumykov.file_lister_navigator_selector.file_lister.FileLister
 import com.github.aakumykov.file_lister_navigator_selector.file_lister.SimpleSortingMode
 import com.github.aakumykov.file_lister_navigator_selector.recursive_dir_reader.RecursiveDirReader
-import com.github.aakumykov.sync_dir_to_cloud.AssistedArgName
 import com.github.aakumykov.sync_dir_to_cloud.di.factories.RecursiveDirReaderFactory
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncObject
 import com.github.aakumykov.sync_dir_to_cloud.enums.StorageType
@@ -11,38 +10,27 @@ import com.github.aakumykov.sync_dir_to_cloud.interfaces.for_repository.sync_obj
 import com.github.aakumykov.sync_dir_to_cloud.interfaces.for_repository.sync_object.SyncObjectReader
 import com.github.aakumykov.sync_dir_to_cloud.interfaces.for_repository.sync_object.SyncObjectUpdater
 import com.github.aakumykov.sync_dir_to_cloud.sync_task_executor.source_reader.interfaces.SourceReader
-import com.github.aakumykov.sync_dir_to_cloud.sync_task_executor.source_reader.interfaces.SourceReaderAssistedFactory
 import com.github.aakumykov.sync_dir_to_cloud.sync_task_executor.source_reader.strategy.ChangesDetectionStrategy
 import com.github.aakumykov.sync_dir_to_cloud.utils.calculateRelativeParentDirPath
-import dagger.assisted.Assisted
-import dagger.assisted.AssistedFactory
-import dagger.assisted.AssistedInject
 
-class LocalSourceReader @AssistedInject constructor(
-    @Assisted(AssistedArgName.AUTH_TOKEN) private val authToken: String,
-    @Assisted(AssistedArgName.TASK_ID) private val taskId: String,
-    @Assisted private val changesDetectionStrategy: ChangesDetectionStrategy,
+abstract class BasicSourceReader(
+    private val taskId: String,
     private val recursiveDirReaderFactory: RecursiveDirReaderFactory,
+    private val changesDetectionStrategy: ChangesDetectionStrategy,
+    private val authToken: String,
     private val syncObjectReader: SyncObjectReader,
     private val syncObjectAdder: SyncObjectAdder,
     private val syncObjectUpdater: SyncObjectUpdater
 )
     : SourceReader
 {
-    @AssistedFactory
-    interface Factory : SourceReaderAssistedFactory {
-        override fun create(@Assisted(AssistedArgName.AUTH_TOKEN) authToken: String,
-                            @Assisted(AssistedArgName.TASK_ID) taskId: String,
-                            @Assisted changesDetectionStrategy: ChangesDetectionStrategy
-        ): LocalSourceReader
-    }
+    protected abstract val storageType: StorageType
 
-    // TODO: в базовый класс...
-    private val recursiveDirReader: RecursiveDirReader? by lazy {
-        recursiveDirReaderFactory.create(StorageType.LOCAL, authToken)
-    }
 
-    // TODO: в базовый класс...
+    private val recursiveDirReader: RecursiveDirReader?
+        get() = recursiveDirReaderFactory.create(storageType, authToken)
+
+
     @Throws(FileLister.NotADirException::class)
     override suspend fun read(sourcePath: String) {
 
---
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/sync_task_executor/source_reader/yandex_source_reader/YandexSourceReader.kt
@@ -0,0 +1,37 @@
+package com.github.aakumykov.sync_dir_to_cloud.sync_task_executor.source_reader.yandex_source_reader
+
+import com.github.aakumykov.sync_dir_to_cloud.AssistedArgName
+import com.github.aakumykov.sync_dir_to_cloud.di.factories.RecursiveDirReaderFactory
+import com.github.aakumykov.sync_dir_to_cloud.enums.StorageType
+import com.github.aakumykov.sync_dir_to_cloud.interfaces.for_repository.sync_object.SyncObjectAdder
+import com.github.aakumykov.sync_dir_to_cloud.interfaces.for_repository.sync_object.SyncObjectReader
+import com.github.aakumykov.sync_dir_to_cloud.interfaces.for_repository.sync_object.SyncObjectUpdater
+import com.github.aakumykov.sync_dir_to_cloud.sync_task_executor.source_reader.BasicSourceReader
+import com.github.aakumykov.sync_dir_to_cloud.sync_task_executor.source_reader.interfaces.SourceReader
+import com.github.aakumykov.sync_dir_to_cloud.sync_task_executor.source_reader.strategy.ChangesDetectionStrategy
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+
+class YandexSourceReader @AssistedInject constructor(
+    @Assisted(AssistedArgName.AUTH_TOKEN) authToken: String,
+    @Assisted(AssistedArgName.TASK_ID) taskId: String,
+    @Assisted changesDetectionStrategy: ChangesDetectionStrategy,
+    private val recursiveDirReaderFactory: RecursiveDirReaderFactory,
+    syncObjectReader: SyncObjectReader,
+    syncObjectAdder: SyncObjectAdder,
+    syncObjectUpdater: SyncObjectUpdater
+)
+    // TODO: делегировать ему, чтобы не вызывать этот громоздкий конструктор.
+    : BasicSourceReader(
+        taskId = taskId,
+        recursiveDirReaderFactory = recursiveDirReaderFactory,
+        changesDetectionStrategy = changesDetectionStrategy,
+        authToken = authToken,
+        syncObjectReader = syncObjectReader,
+        syncObjectUpdater = syncObjectUpdater,
+        syncObjectAdder = syncObjectAdder
+    )
+{
+    override val storageType: StorageType
+        get() = StorageType.YANDEX_DISK
+}
