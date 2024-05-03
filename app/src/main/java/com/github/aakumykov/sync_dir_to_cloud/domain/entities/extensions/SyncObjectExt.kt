6998d037e731f768393fa0532a70c700766f2c0a
Andrey Kumykov <aakumykov@yandex.ru>
2024-05-03 21:44:39 +0300

    TODO: дать имя, точно отражающее функцию в SyncObjectExt.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/domain/entities/extensions/SyncObjectExt.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/domain/entities/extensions/SyncObjectExt.kt
@@ -3,6 +3,7 @@ package com.github.aakumykov.sync_dir_to_cloud.domain.entities.extensions
 import com.github.aakumykov.file_lister_navigator_selector.fs_item.FSItem
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncObject
 
+// TODO: дать имя, точно отражающее функцию.
 fun SyncObject.shiftTwoVersionParameters(modifiedFSItem: FSItem) {
 
     if (null != newSize)
