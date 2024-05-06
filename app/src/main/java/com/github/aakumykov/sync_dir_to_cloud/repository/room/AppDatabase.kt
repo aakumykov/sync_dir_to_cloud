e5dd516361e5410b3660542f22cf0d29c8fa5a73
Andrey Kumykov <aakumykov@yandex.ru>
2024-05-06 18:53:57 +0300

    Переименовывает свойство syncState в executionState в классах SyncTask и SyncObject; версия БД 41
    Чтобы соответствовать своему типу: ExecutionState.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/repository/room/AppDatabase.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/repository/room/AppDatabase.kt
@@ -9,7 +9,7 @@ import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncTask
 
 @Database(
     entities = [ SyncTask::class, SyncObject::class, CloudAuth::class ],
-    version = 40,
+    version = 41,
     autoMigrations = [
 
     ]
