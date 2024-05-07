858c10b52bb3f59919cded2842fe13ba95ac467b
Andrey Kumykov <aakumykov@yandex.ru>
2024-05-07 22:52:08 +0300

    Методы SchedulingSyncTaskUseCase возвращают Result
    Но он пока не используется, так как нужен механизм отправки сообщений об ошибке в интерфейс пользователя.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/domain/use_cases/sync_task/SchedulingSyncTaskUseCase.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/domain/use_cases/sync_task/SchedulingSyncTaskUseCase.kt
@@ -20,51 +20,65 @@ class SchedulingSyncTaskUseCase @Inject constructor(
 ) {
     // TODO: испытать поведение при ошибках
 
-    suspend fun toggleTaskScheduling(taskId: String) {
-        syncTaskReader.getSyncTask(taskId).also { syncTask ->
+    suspend fun toggleTaskScheduling(taskId: String): Result<String> {
+        return syncTaskReader.getSyncTask(taskId).let { syncTask ->
             if (syncTask.isEnabled) unScheduleSyncTask(syncTask)
             else scheduleSyncTask(syncTask)
         }
     }
 
-    suspend fun unScheduleSyncTask(syncTask: SyncTask) {
-        try {
-            // FIXME: ExecutionState.RUNNING плохо подходит для состояния регистрации задачи в планировщике.
-            syncTaskStateChanger.changeSchedulingState(syncTask.id, ExecutionState.RUNNING)
-            syncTaskScheduler.unScheduleSyncTask(syncTask)
-            syncTaskStateChanger.changeSchedulingState(syncTask.id, ExecutionState.NEVER)
-            syncTaskStateChanger.changeSyncTaskEnabled(syncTask.id, false)
-        }
-        catch (t: Throwable) {
-            syncTaskStateChanger.changeSchedulingState(syncTask.id, ExecutionState.ERROR, ExceptionUtils.getErrorMessage(t))
-        }
-    }
-
-    suspend fun updateTaskSchedule(syncTask: SyncTask) {
-        if (syncTask.isEnabled) {
-            // Метод SyncTaskScheduler.scheduleSyncTask() обновляет задачу,
-            // поэтому необязательно предварительно убирать её из планировщика.
-            scheduleSyncTask(syncTask)
-        }
-    }
-
-
-    // TODO: возвращать Result
+    /**
+     * @return Result с taskId внутри.
+     */
     // TODO: возвращать сообщение об ошибке как TextMessage
-    private suspend fun scheduleSyncTask(syncTask: SyncTask) {
-        try {
+    private suspend fun scheduleSyncTask(syncTask: SyncTask): Result<String> {
+        return try {
             if (syncTask.executionIntervalNotZero()) {
                 syncTaskStateChanger.changeSchedulingState(syncTask.id, ExecutionState.RUNNING)
                 syncTaskScheduler.scheduleSyncTask(syncTask)
                 syncTaskStateChanger.changeSchedulingState(syncTask.id, ExecutionState.SUCCESS)
                 syncTaskStateChanger.changeSyncTaskEnabled(syncTask.id, true)
+                Result.success(syncTask.id)
             } else {
                 throw Exception("Scheduling interval cannot be zero.")
             }
         }
         catch (t: Throwable) {
-            syncTaskStateChanger.changeSchedulingState(syncTask.id, ExecutionState.ERROR, ExceptionUtils.getErrorMessage(t))
+            markTaskAsSchedulingError(syncTask.id, t)
+            Result.failure(t)
         }
+    }
+
+    /**
+     * @return Result с taskId внутри.
+     */
+    suspend fun unScheduleSyncTask(syncTask: SyncTask): Result<String> {
+        return try {
+            // FIXME: ExecutionState.RUNNING плохо подходит для состояния регистрации задачи в планировщике.
+            syncTaskStateChanger.changeSchedulingState(syncTask.id, ExecutionState.RUNNING)
+            syncTaskScheduler.unScheduleSyncTask(syncTask)
+            syncTaskStateChanger.changeSchedulingState(syncTask.id, ExecutionState.NEVER)
+            syncTaskStateChanger.changeSyncTaskEnabled(syncTask.id, false)
+            Result.success(syncTask.id)
+        }
+        catch (t: Throwable) {
+            markTaskAsSchedulingError(syncTask.id, t)
+            Result.failure(t)
+        }
+    }
+
+    private suspend fun markTaskAsSchedulingError(taskId: String, t: Throwable) {
+        syncTaskStateChanger.changeSchedulingState(taskId, ExecutionState.ERROR, ExceptionUtils.getErrorMessage(t))
+    }
+
+
+    suspend fun updateTaskSchedule(syncTask: SyncTask): Result<String> {
+        if (syncTask.isEnabled) {
+            // Метод SyncTaskScheduler.scheduleSyncTask() обновляет задачу,
+            // поэтому необязательно предварительно убирать её из планировщика.
+            scheduleSyncTask(syncTask)
+        }
+        return Result.success(syncTask.id)
     }
 
 
