fd1c7d5504601714afa93ac742efd8fe4add3820
Andrey Kumykov <aakumykov@yandex.ru>
2024-05-07 22:53:03 +0300

    Сообщение @Deprecated("Сделать возвращаемые значения nullable") для методов SyncTaskReader

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/interfaces/for_repository/sync_task/SyncTaskReader.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/interfaces/for_repository/sync_task/SyncTaskReader.kt
@@ -4,7 +4,12 @@ import androidx.lifecycle.LiveData
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncTask
 
 interface SyncTaskReader {
+    @Deprecated("Сделать возвращаемые значения nullable")
     suspend fun getSyncTask(id: String): SyncTask
+
+    @Deprecated("Сделать возвращаемые значения nullable")
     suspend fun getSyncTaskAsLiveData(taskId: String): LiveData<SyncTask>
+
+    @Deprecated("Сделать возвращаемые значения nullable")
     suspend fun listSyncTasks(): LiveData<List<SyncTask>>
 }
