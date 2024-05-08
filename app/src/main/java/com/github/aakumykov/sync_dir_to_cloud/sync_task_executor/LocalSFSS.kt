a5094ed3b6b8a01160f89b39521bffda8ec45a1e
Andrey Kumykov <aakumykov@yandex.ru>
2024-05-08 21:23:54 +0300

    Упрощает код LocalSFSS.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/sync_task_executor/LocalSFSS.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/sync_task_executor/LocalSFSS.kt
@@ -3,15 +3,11 @@ package com.github.aakumykov.sync_dir_to_cloud.sync_task_executor
 import com.github.aakumykov.sync_dir_to_cloud.sync_task_executor.source_file_stream.SourceFileStreamSupplier
 import java.io.File
 import java.io.FileInputStream
-import java.io.FileOutputStream
 
 class LocalSFSS : SourceFileStreamSupplier {
     override suspend fun getSourceFileStream(absolutePath: String): Result<FileInputStream> {
         return try {
-            val file = File(absolutePath)
-//            val is1 = FileInputStream(file) // Странное предупреждение в сравнении с file.inputStream()
-            val is2 = file.inputStream()
-            Result.success(is2)
+            Result.success(File(absolutePath).inputStream())
         } catch (e: Exception) {
             Result.failure(e)
         }
