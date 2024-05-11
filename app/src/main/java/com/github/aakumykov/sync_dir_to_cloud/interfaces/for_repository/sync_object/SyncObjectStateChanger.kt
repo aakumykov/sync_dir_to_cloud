df20fb5ec0059fd2f6a6fd1005c87c7bc6b691c5
Andrey Kumykov <aakumykov@yandex.ru>
2024-05-11 08:36:52 +0300

    SyncObjectStateChanger: TODO: дать более осмысленное название / разделить на отдельные интерфейсы

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/interfaces/for_repository/sync_object/SyncObjectStateChanger.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/interfaces/for_repository/sync_object/SyncObjectStateChanger.kt
@@ -3,6 +3,7 @@ package com.github.aakumykov.sync_dir_to_cloud.interfaces.for_repository.sync_ob
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.ModificationState
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.ExecutionState
 
+// TODO: дать более осмысленное название / разделить на отдельные интерфейсы
 interface SyncObjectStateChanger {
     suspend fun changeExecutionState(objectId: String, syncState: ExecutionState, errorMsg: String = "")
     // TODO: переименовать в setSyncTime()
