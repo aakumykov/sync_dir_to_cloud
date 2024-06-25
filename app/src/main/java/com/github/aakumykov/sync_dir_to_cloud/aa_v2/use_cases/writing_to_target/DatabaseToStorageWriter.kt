16c882ca52e7e052afb32bdf077b1353bac02f3c
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-25 19:22:00 +0300

    Набросал вспомогательные классы для DatabaseToStorageWriter-а.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/aa_v2/use_cases/writing_to_target/DatabaseToStorageWriter.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/aa_v2/use_cases/writing_to_target/DatabaseToStorageWriter.kt
@@ -1,13 +1,43 @@
 package com.github.aakumykov.sync_dir_to_cloud.aa_v2.use_cases.writing_to_target
 
+import com.github.aakumykov.file_lister_navigator_selector.dir_creator.DirCreator
+import com.github.aakumykov.sync_dir_to_cloud.aa_v2.use_cases.writing_to_target.dirs.DeletedDirsDeleter
+import com.github.aakumykov.sync_dir_to_cloud.aa_v2.use_cases.writing_to_target.dirs.DeletedFilesDeleter
+import com.github.aakumykov.sync_dir_to_cloud.aa_v2.use_cases.writing_to_target.dirs.LostDirsCreator
+import com.github.aakumykov.sync_dir_to_cloud.aa_v2.use_cases.writing_to_target.dirs.LostFilesCopier
+import com.github.aakumykov.sync_dir_to_cloud.aa_v2.use_cases.writing_to_target.dirs.ModifiedFilesCopier
+import com.github.aakumykov.sync_dir_to_cloud.aa_v2.use_cases.writing_to_target.dirs.NewDirsCreator
+import com.github.aakumykov.sync_dir_to_cloud.aa_v2.use_cases.writing_to_target.dirs.NewFilesCopier
+import com.github.aakumykov.sync_dir_to_cloud.domain.entities.CloudAuth
+import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncTask
 import javax.inject.Inject
 
 class DatabaseToStorageWriter @Inject constructor(
-
+    // Каталоги
+    private val deletedDirsDeleter: DeletedDirsDeleter,
+    private val newDirsCreator: NewDirsCreator,
+    private val lostDirsCreator: LostDirsCreator,
+    // Файлы
+    private val deletedFilesDeleter: DeletedFilesDeleter,
+    private val newFilesCopier: NewFilesCopier,
+    private val modifiedFilesCopier: ModifiedFilesCopier,
+    private val lostFilesCopier: LostFilesCopier,
 ) {
-    /* I. Синхронизировать каталоги
-            1. Удалить удалённое (зависит от стратегии)
-            2. Копировать изменившееся (стратегия)
-            3. Восстановить пропавшее (стратегия)
-       II. Синхронизировать файлы*/
+    /*
+        I. Синхронизировать каталоги
+            1. Удалить удалённые каталоги (стратегия)
+            2. Создать новые каталоги (стратегия)
+            3. Восстановить пропавшие каталоги (стратегия)
+       II. Синхронизировать файлы
+            1. Удалить удалённое (стратегия)
+            2. Копировать новое (стратегия)
+            3. Копировать изменившееся (стратегия)
+            4. Восстановить пропавшее (стратегия)
+   */
+    fun writeToPath(taskId: String,
+//                    syncMode: SyncMode,
+                    sourceAuth: CloudAuth,
+                    targetAuth: CloudAuth) {
+
+    }
 }
---
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/aa_v2/use_cases/writing_to_target/dirs/DeletedFilesDeleter.kt
@@ -0,0 +1,5 @@
+package com.github.aakumykov.sync_dir_to_cloud.aa_v2.use_cases.writing_to_target.dirs
+
+class DeletedFilesDeleter {
+
+}
