3f48e6683aa73033b8162f6a6ef1b09ff05d9b93
Andrey Kumykov <aakumykov@yandex.ru>
2024-08-09 16:52:48 +0300

    TODO: Сделать синглтоном для SyncTaskLogger

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/sync_task_logger/SyncTaskLogger.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/sync_task_logger/SyncTaskLogger.kt
@@ -2,6 +2,7 @@ package com.github.aakumykov.sync_dir_to_cloud.sync_task_logger
 
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.TaskLogEntry
 
+@Deprecated("TODO: Сделать синглтоном")
 interface SyncTaskLogger {
     suspend fun log(taskLogEntry: TaskLogEntry)
 }
