0c4db9c4f33ef6f3982f30d01f064ac1b056f9a2
Andrey Kumykov <aakumykov@yandex.ru>
2024-08-13 14:15:02 +0300

    Название операции, имя файла и иконка статуса в журнале синхронизации.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/sync_log/SyncLogViewHolder.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/sync_log/SyncLogViewHolder.kt
@@ -1,6 +1,7 @@
 package com.github.aakumykov.sync_dir_to_cloud.view.sync_log
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import com.github.aakumykov.list_holding_list_adapter.ListHoldingListAdapter
 import com.github.aakumykov.sync_dir_to_cloud.R
@@ -8,13 +9,19 @@ import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncObjectLogItem
 
 class SyncLogViewHolder : ListHoldingListAdapter.ViewHolder<SyncObjectLogItem>() {
 
-    private lateinit var titleView: TextView;
+    private lateinit var nameView: TextView
+    private lateinit var messageView: TextView
+    private lateinit var stateIconView: ImageView
 
     override fun init(itemView: View) {
-        titleView = itemView.findViewById(R.id.syncLogTitleView)
+        nameView = itemView.findViewById(R.id.syncLogNameView)
+        messageView = itemView.findViewById(R.id.syncLogMessageView)
+        stateIconView = itemView.findViewById(R.id.syncLogStateIconView)
     }
 
     override fun fill(item: SyncObjectLogItem, isSelected: Boolean) {
-        titleView.text = "${item.name} [${item.message}]"
+        nameView.text = item.name
+        messageView.text = item.message
+        stateIconView.setImageResource(if (item.isSuccessful) R.drawable.ic_sync_log_success else R.drawable.ic_sync_log_error)
     }
 }
