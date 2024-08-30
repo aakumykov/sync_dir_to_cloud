6eff13d03141869f8bf87261fa2bc2be1057ccc1
Andrey Kumykov <aakumykov@yandex.ru>
2024-08-30 16:32:53 +0300

    fix: подсчёт прогресса передачи файла при нулевом размере

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/aa_v2/use_cases/v3/copy_files/SyncObjectFileCopier.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/aa_v2/use_cases/v3/copy_files/SyncObjectFileCopier.kt
@@ -1,14 +1,13 @@
 package com.github.aakumykov.sync_dir_to_cloud.aa_v2.use_cases.v3.copy_files
 
-import android.util.Log
 import com.github.aakumykov.cloud_writer.CloudWriter
-import com.github.aakumykov.sync_dir_to_cloud.utils.CountingBufferedInputStream
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncObject
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncTask
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.extensions.actualSize
 import com.github.aakumykov.sync_dir_to_cloud.extensions.absolutePathIn
 import com.github.aakumykov.sync_dir_to_cloud.extensions.round
 import com.github.aakumykov.sync_dir_to_cloud.source_file_stream_supplier.SourceFileStreamSupplier
+import com.github.aakumykov.sync_dir_to_cloud.utils.CountingBufferedInputStream
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -18,6 +17,7 @@ import java.io.InputStream
 /**
  * Копирует данные SyncObject-а из источника в приёмник указанный в SyncTask.
  */
+// TODO: внедрять объект-считатель прогресса
 class SyncObjectFileCopier (
     private val sourceFileStreamSupplier: SourceFileStreamSupplier,
     private val cloudWriter: CloudWriter,
@@ -40,13 +40,18 @@ class SyncObjectFileCopier (
 
             val countingInputStream = CountingBufferedInputStream(sourceFileStream) { readCount ->
 
-                val progress = (1f*readCount / syncObject.actualSize).round(2)
+                // Если размер файла 0, приходит "считано 0" и прогресс сразу становится 100%.
+                val progress = if (0L == syncObject.actualSize) 1.0f
+                               else (1f*readCount / syncObject.actualSize).round(2)
+
                 val progressAsPartOf100 = Math.round(progress * 100)
 
+                // Реализация пропуска повторяющихся значений
                 if (lastProgressValue != progress) {
-                    Log.d(TAG, "progress: ${syncObject.name} - $progress ($progressAsPartOf100) [$readCount / ${syncObject.actualSize}]")
                     lastProgressValue = progress
-                    CoroutineScope(progressCallbackCoroutineDispatcher).launch { onProgressChanged.invoke(progressAsPartOf100) }
+                    CoroutineScope(progressCallbackCoroutineDispatcher).launch {
+                        onProgressChanged.invoke(progressAsPartOf100)
+                    }
                 }
             }
 
