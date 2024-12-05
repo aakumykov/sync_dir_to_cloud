96915a281211f2af5c0897f227e4e602849e5513
Andrey Kumykov <aakumykov@yandex.ru>
2024-12-05 13:28:09 +0300

    Новый метод ExecutionLogDAO.updateItem(...)

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/repository/room/dao/ExecutionLogDAO.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/repository/room/dao/ExecutionLogDAO.kt
@@ -4,21 +4,30 @@ import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Update
+import androidx.room.Transaction
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.ExecutionLogItem
+import com.github.aakumykov.sync_dir_to_cloud.enums.ExecutionLogItemType
 
 @Dao
-interface ExecutionLogDAO {
+abstract class ExecutionLogDAO {
 
     @Insert
-    suspend fun addItem(executionLogItem: ExecutionLogItem)
+    abstract suspend fun addItem(executionLogItem: ExecutionLogItem)
 
-    @Update
-    suspend fun updateItem(executionLogItem: ExecutionLogItem)
+    suspend fun updateItem(item: ExecutionLogItem) {
+        updateItemReal(taskId = item.taskId,
+            executionId = item.executionId,
+            timestamp = item.timestamp,
+            type = item.type,
+        )
+    }
+
+    @Query("UPDATE execution_log SET timestamp = :timestamp, type = :type WHERE task_id = :taskId AND execution_id = :executionId")
+    protected abstract suspend fun updateItemReal(taskId: String, executionId: String, timestamp: Long, type: ExecutionLogItemType)
 
     @Query("SELECT * FROM execution_log WHERE task_id = :taskId AND execution_id = :executionId")
-    fun getLogsAsLiveData(taskId: String, executionId: String): LiveData<List<ExecutionLogItem>>
+    abstract fun getLogsAsLiveData(taskId: String, executionId: String): LiveData<List<ExecutionLogItem>>
 
     @Query("DELETE FROM execution_log")
-    fun clear()
+    abstract fun clear()
 }
