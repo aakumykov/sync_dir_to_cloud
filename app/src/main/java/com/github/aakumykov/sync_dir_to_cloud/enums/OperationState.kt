c2122d05232b701ecdd050e859dadfba71322e57
Andrey Kumykov <aakumykov@yandex.ru>
2024-12-16 12:00:35 +0300

    Метод OperationState.isError()

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/enums/OperationState.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/enums/OperationState.kt
@@ -1,3 +1,6 @@
 package com.github.aakumykov.sync_dir_to_cloud.enums
 
-enum class OperationState { WAITING, RUNNING, PAUSED, SUCCESS, ERROR }
+enum class OperationState {
+                          WAITING, RUNNING, PAUSED, SUCCESS, ERROR;
+    fun isError(): Boolean = ERROR == this
+}
