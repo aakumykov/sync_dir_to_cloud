3ba9d68f36a12d228e16e307e2426ac3e174c8df
Andrey Kumykov <aakumykov@yandex.ru>
2025-02-17 00:54:39 +0700

    SyncObjectListChunkedCopier5

---
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/aa_v5/level_06_sync_object_list/SyncObjectListChunkedCopier5.kt
@@ -0,0 +1,62 @@
+package com.github.aakumykov.sync_dir_to_cloud.aa_v5.level_06_sync_object_list
+
+import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncObject
+import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncTask
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class SyncObjectListChunkedCopier5 @AssistedInject constructor(
+    @Assisted private val syncTask: SyncTask,
+    @Assisted private val chunkSize: Int,
+    @Assisted private val scope: CoroutineScope,
+    private val syncObjectListCopierAssistedFactory5: SyncObjectListCopierAssistedFactory5,
+) {
+    // S --> T
+    suspend fun copySyncObjectListFromSourceToTargetByChunksInScope(
+        list: List<SyncObject>,
+        overwriteIfExists: Boolean
+    ): Job {
+        return scope.launch {
+            copySyncObjectListFromSourceToTargetByChunks(list, overwriteIfExists)
+        }
+    }
+
+    // S <-- T
+    suspend fun copySyncObjectListFromTargetToSourceByChunksInScope(
+        list: List<SyncObject>,
+        overwriteIfExists: Boolean
+    ): Job {
+        return scope.launch {
+            copySyncObjectListFromTargetToSourceByChunks(list, overwriteIfExists)
+        }
+    }
+
+
+    // S --> T
+    suspend fun copySyncObjectListFromSourceToTargetByChunks(list: List<SyncObject>, overwriteIfExists: Boolean) {
+        list.chunked(chunkSize).forEach { chunk ->
+            syncObjectListCopier.copyListFromSourceToTarget(chunk, overwriteIfExists)
+        }
+    }
+
+    // S <-- T
+    suspend fun copySyncObjectListFromTargetToSourceByChunks(list: List<SyncObject>, overwriteIfExists: Boolean) {
+        list.chunked(chunkSize).forEach { chunk ->
+            syncObjectListCopier.copyListFromTargetToSource(chunk, overwriteIfExists)
+        }
+    }
+
+
+    private val syncObjectListCopier: SyncObjectListCopier5
+        get() = syncObjectListCopierAssistedFactory5.create(syncTask, scope)
+}
+
+
+@AssistedFactory
+interface SyncObjectListChunkedCopierAssistedFactory5 {
+    fun create(syncTask: SyncTask, chunkSize: Int, scope: CoroutineScope): SyncObjectListChunkedCopier5
+}
