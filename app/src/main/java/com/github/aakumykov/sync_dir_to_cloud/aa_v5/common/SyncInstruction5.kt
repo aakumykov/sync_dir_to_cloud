9d6cec14efcc426a94d087321901756f5c4988d1
Andrey Kumykov <aakumykov@yandex.ru>
2025-03-01 13:07:28 +0700

    Переименовал SyncInstruction5.orderNum --> groupOrderNum, версия БД 90

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/aa_v5/common/SyncInstruction5.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/aa_v5/common/SyncInstruction5.kt
@@ -5,6 +5,8 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.ForeignKey.Companion.CASCADE
 import androidx.room.PrimaryKey
+import androidx.room.RenameColumn
+import androidx.room.migration.AutoMigrationSpec
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncTask
 import com.github.aakumykov.sync_dir_to_cloud.enums.SyncSide
 
@@ -26,10 +28,13 @@ class SyncInstruction5 (
     @ColumnInfo(name = "task_id", index = true) val taskId: String,
     @ColumnInfo(name = "execution_id") val executionId: String,
     @ColumnInfo(name = "execution_order_num", defaultValue = "0") val executionOrderNum: Int,
-    @ColumnInfo(name = "order_num") val orderNum: Int,
+    @ColumnInfo(name = "group_order_num", defaultValue = "0") val groupOrderNum: Int,
     @ColumnInfo(name = "object_id") val objectId: String,
     @ColumnInfo(name = "sync_side") val syncSide: SyncSide,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "is_dir") val isDir: Boolean,
     @ColumnInfo(name = "sync_operation") val operation: SyncOperation,
-)
+) {
+    @RenameColumn(tableName = "sync_instructions_5", fromColumnName = "order_num", toColumnName = "execution_order_num")
+    class RenameOrderNumToGroupOrderNumMigrationSpec : AutoMigrationSpec
+}
