cd0fa0b798edabb9ed38d4a8a07912f0ecf299f3
Andrey Kumykov <aakumykov@yandex.ru>
2025-03-05 22:03:39 +0700

    fix: InputStreamGetter5 использовал путь к файлу в приёмнике, а нужно в источнике

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/aa_v5/level_03_intermediate/InputStreamGetter5.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/aa_v5/level_03_intermediate/InputStreamGetter5.kt
@@ -5,6 +5,7 @@ import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncObject
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncTask
 import com.github.aakumykov.sync_dir_to_cloud.extensions.absolutePathIn
 import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import java.io.InputStream
 
@@ -14,7 +15,7 @@ class InputStreamGetter5 @AssistedInject constructor(
 ) {
     @Throws(Exception::class)
     suspend fun getInputStreamFor(syncObject: SyncObject): InputStream {
-        val filePath = syncObject.absolutePathIn(syncTask.targetPath!!)
+        val filePath = syncObject.absolutePathIn(syncTask.sourcePath!!)
         return cloudReaderGetter
             .getSourceCloudReaderFor(syncTask)
             .getFileInputStream(filePath)
@@ -22,6 +23,7 @@ class InputStreamGetter5 @AssistedInject constructor(
     }
 }
 
+@AssistedFactory
 interface InputStreamGetterAssistedFactory5 {
     fun create(syncTask: SyncTask): InputStreamGetter5
 }
