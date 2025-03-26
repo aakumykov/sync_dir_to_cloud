9291ff958dd537c5990eba26d792c2a4a649d1e9
Andrey Kumykov <aakumykov@yandex.ru>
2025-03-26 16:30:14 +0300

    Заполнил полями SyncOperationLogItem, установил имя таблицы ("sync_operation_logs").

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/domain/entities/SyncOperationLogItem.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/domain/entities/SyncOperationLogItem.kt
@@ -1,7 +1,33 @@
 package com.github.aakumykov.sync_dir_to_cloud.domain.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Entity
-class SyncOperationLogItem {
-}
+@Entity(tableName = "sync_operation_logs")
+class SyncOperationLogItem(
+
+    @PrimaryKey
+    val id: String,
+
+    @ColumnInfo(name = "task_id")
+    val taskId: String,
+
+    @ColumnInfo(name = "execution_id")
+    val executionId: String,
+
+    @ColumnInfo(name = "timestamp")
+    val timestamp: String,
+
+    @ColumnInfo(name = "source_object_id")
+    val sourceObjectId: String?,
+
+    @ColumnInfo(name = "target_object_id")
+    val targetObjectId: String?,
+
+    @ColumnInfo(name = "object_name")
+    val objectName: String,
+
+    @ColumnInfo(name = "operation_name")
+    val operationName: String,
+)
