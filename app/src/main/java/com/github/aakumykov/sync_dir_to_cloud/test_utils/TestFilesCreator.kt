8ad675ee702e691e5580c96df89650b148cb8725
Andrey Kumykov <aakumykov@yandex.ru>
2025-04-03 14:57:52 +0300

    Начал делать TestFilesCreator, но понял, что нужно использовать Dagger2.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/test_utils/TestFilesCreator.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/test_utils/TestFilesCreator.kt
@@ -1,5 +1,10 @@
 package com.github.aakumykov.sync_dir_to_cloud.test_utils
 
-class TestFilesCreator {
+import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncTask
 
+class TestFilesCreator () {
+
+    suspend fun prepareSourceOnlyFile(taskId: String) {
+
+    }
 }
