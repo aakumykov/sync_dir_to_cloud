5ae771d793ed2bd696d4dac2655d2682c9638997
Andrey Kumykov <aakumykov@yandex.ru>
2025-04-11 22:40:38 +0300

    Наполняю модулями TestComponent, понимая, что нужно разбивать на подкомпоненты, а то и всё приложение на модули.

--- app/src/androidTest/java/com/github/aakumykov/sync_dir_to_cloud/bb_new/di/TestComponent.kt
+++ app/src/androidTest/java/com/github/aakumykov/sync_dir_to_cloud/bb_new/di/TestComponent.kt
@@ -3,15 +3,29 @@ package com.github.aakumykov.sync_dir_to_cloud.bb_new.di
 import com.github.aakumykov.sync_dir_to_cloud.bb_new.di.modules.TestDaoModule
 import com.github.aakumykov.sync_dir_to_cloud.bb_new.di.modules.TestContextModule
 import com.github.aakumykov.sync_dir_to_cloud.di.AppComponent
+import com.github.aakumykov.sync_dir_to_cloud.di.annotations.AppScope
+import com.github.aakumykov.sync_dir_to_cloud.di.annotations.ExecutionScope
+import com.github.aakumykov.sync_dir_to_cloud.di.modules.CloudAuthRepositoryInterfacesModule
+import com.github.aakumykov.sync_dir_to_cloud.di.modules.GsonModule
+import com.github.aakumykov.sync_dir_to_cloud.di.modules.RoomDAOModule
+import com.github.aakumykov.sync_dir_to_cloud.di.modules.SyncObjectRepositoryInterfacesModule
+import com.github.aakumykov.sync_dir_to_cloud.di.modules.SyncTaskLoggerInterfacesModule
+import com.github.aakumykov.sync_dir_to_cloud.di.modules.SyncTaskRepositoryInterfacesModule
 import dagger.Component
 
 @Component(
     modules = [
         TestContextModule::class,
         TestDaoModule::class,
+        CloudAuthRepositoryInterfacesModule::class,
+        SyncTaskRepositoryInterfacesModule::class,
+        SyncObjectRepositoryInterfacesModule::class,
+        RoomDAOModule::class,
+        GsonModule::class,
     ]
 )
+@AppScope
+@ExecutionScope
 abstract class TestComponent : AppComponent {
-
 
 }
