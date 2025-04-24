7f746a222255a25cd4f8e76e5808818ef480dc4b
Andrey Kumykov <aakumykov@yandex.ru>
2025-04-24 18:24:12 +0300

    Удалил FileDeleter.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/file_deleter/FileDeleter.kt
+++
@@ -1,7 +0,0 @@
-package com.github.aakumykov.sync_dir_to_cloud.file_deleter
-
-import dagger.assisted.AssistedInject
-import javax.inject.Inject
-
-class FileDeleter @AssistedInject constructor(){
-}
