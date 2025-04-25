92c2a2506292a7c5f99c9a05259f07b22410004d
Andrey Kumykov <aakumykov@yandex.ru>
2025-04-25 17:49:17 +0300

    Удалил файл ProcessingCallbacksAliases.kt

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/aa_v2/use_cases/v3/ProcessingCallbacksAliases.kt
+++
@@ -1,7 +0,0 @@
-package com.github.aakumykov.sync_dir_to_cloud.aa_v2.use_cases.v3
-
-import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncObject
-
-typealias OnSyncObjectProcessingBegin = suspend (syncObject: SyncObject) -> Unit
-typealias OnSyncObjectProcessingSuccess = suspend (syncObject: SyncObject) -> Unit
-typealias OnSyncObjectProcessingFailed = suspend (syncObject: SyncObject, throwable: Throwable) -> Unit
