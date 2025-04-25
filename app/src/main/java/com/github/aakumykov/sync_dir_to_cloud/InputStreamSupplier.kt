d311edc6426fd2e40355cb1a722c744d1b597bc1
Andrey Kumykov <aakumykov@yandex.ru>
2025-04-25 13:16:54 +0300

    Переместил InputStreamSupplier временно в корень.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/InputStreamSupplier.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/InputStreamSupplier.kt
@@ -1,4 +1,4 @@
-package com.github.aakumykov.sync_dir_to_cloud.sync_task_executor
+package com.github.aakumykov.sync_dir_to_cloud
 
 import com.github.aakumykov.sync_dir_to_cloud.di.creators.CloudReadersHolder
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncTask
