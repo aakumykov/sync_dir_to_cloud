f5d7f0538b3ccb4c8a0af01ba3a8e68318ca6753
Andrey Kumykov <aakumykov@yandex.ru>
2025-06-03 17:15:37 +0300

    Новые функции dirNameFromPath(), basePathOf()

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/functions/combine_fs_paths.kt
+++
@@ -1,7 +0,0 @@
-package com.github.aakumykov.sync_dir_to_cloud.functions
-
-import java.io.File
-
-fun combineFSPaths(parentPath: String, childPath: String): String {
-    return File(parentPath, childPath).absolutePath
-}
--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/functions/file_name_from_path.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/functions/file_name_from_path.kt
@@ -2,4 +2,15 @@ package com.github.aakumykov.sync_dir_to_cloud.functions
 
 import java.io.File
 
-fun fileNameFromPath(path: String): String = File(path).name
+fun combineFSPaths(parentPath: String, childPath: String): String
+    = File(parentPath, childPath).absolutePath
+
+fun fileNameFromPath(path: String): String
+    = File(path).name
+
+fun dirNameFromPath(path: String): String
+    = fileNameFromPath(path)
+
+fun basePathOf(path: String): String
+    = File(path).parent ?: "/"
+
