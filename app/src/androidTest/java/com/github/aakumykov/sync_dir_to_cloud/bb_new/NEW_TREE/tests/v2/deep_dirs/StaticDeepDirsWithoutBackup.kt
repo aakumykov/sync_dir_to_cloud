73339708e8a4ce5b4e2b07f9ab02ebe0aae5020f
Andrey Kumykov <aakumykov@yandex.ru>
2025-10-16 22:02:53 +0300

    _deep_dirs/ --> deep_dirs/

--- app/src/androidTest/java/com/github/aakumykov/sync_dir_to_cloud/bb_new/NEW_TREE/tests/v2/deep_dirs/StaticDeepDirsWithoutBackup.kt
+++ app/src/androidTest/java/com/github/aakumykov/sync_dir_to_cloud/bb_new/NEW_TREE/tests/v2/deep_dirs/StaticDeepDirsWithoutBackup.kt
@@ -1,4 +1,4 @@
-package com.github.aakumykov.sync_dir_to_cloud.bb_new.NEW_TREE.tests.v2._deep_dirs
+package com.github.aakumykov.sync_dir_to_cloud.bb_new.NEW_TREE.tests.v2.deep_dirs
 
 import com.github.aakumykov.sync_dir_to_cloud.bb_new.config.task_config.LocalToLocalSyncWithoutBackupTaskConfig
 import com.github.aakumykov.sync_dir_to_cloud.bb_new.config.task_config.TaskConfig
