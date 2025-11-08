f9676e723dfc5bc54f25a69ce8c8e2d7dc39f87f
Andrey Kumykov <aakumykov@yandex.ru>
2025-11-08 22:02:55 +0300

    Удалил лишнюю константу из BackupConfig.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/config/BackupConfig.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/config/BackupConfig.kt
@@ -2,7 +2,6 @@ package com.github.aakumykov.sync_dir_to_cloud.config
 
 class BackupConfig {
     companion object {
-        @Deprecated("Не нужно") const val BACKUP_DIR_BASE_PATH = "/"
         const val BACKUPS_TOP_DIR_PREFIX = "BACKUPS"
         const val BACKUPS_DIR_PREFIX = "backup"
         const val BACKUP_DIR_DATE_TIME_FORMAT = "yyyy.MM.dd_HH-mm-ss"
