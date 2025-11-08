4576ddcac08e276045c30c2f099a5e184ce88551
Andrey Kumykov <aakumykov@yandex.ru>
2025-11-08 22:03:36 +0300

    *

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/config/BackupConfig.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/config/BackupConfig.kt
@@ -5,6 +5,7 @@ class BackupConfig {
         const val BACKUPS_TOP_DIR_PREFIX = "BACKUPS"
         const val BACKUPS_DIR_PREFIX = "backup"
         const val BACKUP_DIR_DATE_TIME_FORMAT = "yyyy.MM.dd_HH-mm-ss"
+        // TODO: тестировать!
         const val BACKUP_DIR_CREATION_MAX_ATTEMPTS_COUNT = 5
     }
 }
