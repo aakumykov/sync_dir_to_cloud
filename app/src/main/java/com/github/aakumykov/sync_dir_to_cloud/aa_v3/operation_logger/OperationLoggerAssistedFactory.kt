dca68be902d9df42b958d4fc6aeabb0b831d147b
aakumykov <aakumykov@yandex.ru>
2025-11-11 18:12:47 +0300

    Удалил OperationLogger и его фабрику.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/aa_v3/operation_logger/OperationLoggerAssistedFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.github.aakumykov.sync_dir_to_cloud.aa_v3.operation_logger
-
-import com.github.aakumykov.sync_dir_to_cloud.aa_v3.SyncObjectLogger
-import dagger.assisted.AssistedFactory
-
-@AssistedFactory
-interface OperationLoggerAssistedFactory {
-    fun create(syncObjectLogger: SyncObjectLogger): OperationLogger
-}
