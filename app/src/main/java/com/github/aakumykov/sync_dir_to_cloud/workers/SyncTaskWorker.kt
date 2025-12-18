93df323429dc3896b88b52943b2218e4363f36d9
aakumykov <aakumykov@yandex.ru>
2025-12-18 11:31:17 +0300

    Добавил обработку taskId == null в SyncTaskWorker.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/workers/SyncTaskWorker.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/workers/SyncTaskWorker.kt
@@ -6,34 +6,42 @@ import androidx.work.CoroutineWorker
 import androidx.work.Data
 import androidx.work.WorkerParameters
 import com.github.aakumykov.sync_dir_to_cloud.appComponent
-import com.github.aakumykov.sync_dir_to_cloud.cancellation_holders.TaskCancellationHolder
 import com.github.aakumykov.sync_dir_to_cloud.extensions.errorMsgExtended
-import com.github.aakumykov.sync_dir_to_cloud.interfaces.for_repository.sync_task.SyncTaskReader
 import com.github.aakumykov.sync_dir_to_cloud.utils.SampleService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
-import java.util.concurrent.CancellationException
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentMap
 
-//
+
 // FIXME: пишут, что на работу этому "воркеру" даётся 10 минут:
 //  https://developer.android.com/reference/kotlin/androidx/work/CoroutineWorker
-//
+
+/**
+ * Задача Worker-а - запустить задачу на выполнение, сопроводив это отображением уведомления
+ * (которое, уведомление) используется для того, чтобы worker не умирал при закрытии экрана.
+ */
 class SyncTaskWorker(context: Context, workerParameters: WorkerParameters) : CoroutineWorker(context, workerParameters) {
 
+    // TODO: внедрять диспетчер
     private val coroutineDispatcher = Dispatchers.IO
+
+    private val rawTaskId: String? get() = inputData.getString(KEY_TASK_ID)
+    private val taskId: String get() = rawTaskId!!
+
     private val thisObjectHashCode: String = hashCode().toString()
-    // FIXME: как быть с null? По идее, нужно регистрировать это как ошибку и завершать
-    // задачу как "успешную", чтобы бессмысленно не пытаться выполнить её много раз.
-    // Т.е. нужен доп статус спец. для этой ситуации...
-    private val taskId: String get() = inputData.getString(KEY_TASK_ID)!!
 
 
     override suspend fun doWork(): Result {
         Log.d(TAG, "[worker: $thisObjectHashCode]: doWork()")
+
+        if (null == rawTaskId) {
+            // TODO: показывать уведомление об ошибке
+            Log.e(TAG, "!!! There is no TASK_ID argument passed to $TAG. Cannot work. !!!")
+            return Result.success()
+        }
 
         return CoroutineScope(coroutineDispatcher).async (coroutineDispatcher) {
             doWorkReal(this)
@@ -49,13 +57,15 @@ class SyncTaskWorker(context: Context, workerParameters: WorkerParameters) : Cor
 
             appComponent.getSyncTaskExecutorAssistedFactory().create(coroutineScope).also { syncTaskExecutor ->
                 Log.d(TAG, "[worker: $thisObjectHashCode] Задача '$taskId' начала выполнение, taskJobsHolder: ${taskJobsHolder.hashCode()}, operationJobsHolder: ${operationJobsHolder.hashCode()}")
+
                 syncTaskExecutor.executeSyncTask(taskId)
+
                 Log.d(TAG, "[worker: $thisObjectHashCode]: Задача '$taskId' завершила выполнение, taskJobsHolder: ${taskJobsHolder.hashCode()}, operationJobsHolder: ${operationJobsHolder.hashCode()}")
             }
             Result.success()
         }
         catch (t: Throwable) {
-            Log.e(TAG, "[worker: $thisObjectHashCode] ${e.errorMsgExtended} [worker:$thisObjectHashCode]")
+            Log.e(TAG, "[worker: $thisObjectHashCode] ${t.errorMsgExtended} [worker:$thisObjectHashCode]")
             Log.e(TAG, t.errorMsgExtended)
             return Result.success()
         }
@@ -71,12 +81,13 @@ class SyncTaskWorker(context: Context, workerParameters: WorkerParameters) : Cor
         val TAG: String = SyncTaskWorker::class.java.simpleName
 
         const val KEY_TASK_ID: String = "TASK_ID"
-        const val KEY_ERROR_MSG: String = "ERROR_MSG"
-        const val KEY_SUMMARY: String = "SUMMARY"
 
         fun dataWithTaskId(taskId: String): Data = Data.Builder().putString(KEY_TASK_ID, taskId).build()
 
+        @Deprecated("разобраться, где это держать")
         val taskJobsHolder = TaskJobsHolder
+
+        @Deprecated("разобраться, где это держать")
         val operationJobsHolder = OperationJobsHolder
     }
 }
@@ -130,5 +141,8 @@ object OperationJobsHolder {
     }
 }
 
+@Deprecated("разобраться, где это держать")
 val taskJobsHolder: TaskJobsHolder get() = SyncTaskWorker.taskJobsHolder
+
+@Deprecated("разобраться, где это держать")
 val operationJobsHolder: OperationJobsHolder get() = SyncTaskWorker.operationJobsHolder
