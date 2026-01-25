9c2686efefa3eb61be39eb5cd1930fc95178a558
Andrey Kumykov <aakumykov@yandex.ru>
2026-01-25 22:12:11 +0300

    Опять сложность-путаница.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/aa_v5/level_90_instructions/TaskOneStageExecutor.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/aa_v5/level_90_instructions/TaskOneStageExecutor.kt
@@ -3,6 +3,7 @@ package com.github.aakumykov.sync_dir_to_cloud.aa_v5.level_90_instructions
 import android.util.Log
 import com.github.aakumykov.sync_dir_to_cloud.QUALIFIER_EXECUTION_ID
 import com.github.aakumykov.sync_dir_to_cloud.QUALIFIER_TASK_ID
+import com.github.aakumykov.sync_dir_to_cloud.aa_v5.level_90_instructions.TaskOneStageExecutor.Companion.TAG
 import com.github.aakumykov.sync_dir_to_cloud.extensions.errorMsgExtended
 import com.github.aakumykov.sync_dir_to_cloud.loggers2.instruction_logger.InstructionLogger
 import com.github.aakumykov.sync_dir_to_cloud.loggers2.instruction_logger.InstructionLoggerAssistedFactory
@@ -79,4 +80,59 @@ interface TaskOneStageExecutorAssistedFactory {
         @Assisted(QUALIFIER_EXECUTION_ID) executionId: String,
         scope: CoroutineScope,
     ): TaskOneStageExecutor
+}
+
+
+abstract class CodeBlockExecutor(
+    private val scope: CoroutineScope,
+    private val onStart: () -> Unit,
+    private val onFinish: () -> Unit,
+    private val onCancelled: (e: CancellationException) -> Unit,
+    private val onCriticalError: (t: Throwable) -> Unit,
+    private val onNonCriticalError: (t: Throwable) -> Unit,
+) {
+    suspend fun process(
+        isCritical: Boolean,
+        logMessage: TextMessage,
+        codeBlock: suspend () -> Unit,
+    ) {
+        scope.launch {
+            try {
+                onStart.invoke()
+
+                val nonCriticalExceptionHandler = CoroutineExceptionHandler { context, throwable ->
+                    onNonCriticalError.invoke(throwable)
+                }
+
+                if (isCritical) scope.launch {
+                    codeBlock.invoke()
+                }.join()
+                else {
+                    scope.launch {
+                        supervisorScope {
+                            launch (nonCriticalExceptionHandler) {
+                                codeBlock.invoke()
+                            }.join()
+                        }
+                    }.join()
+                }
+
+                onFinish.invoke()
+
+            } catch (e: CancellationException) {
+                onCancelled.invoke(e)
+                throw e
+            }
+            catch (throwable: Throwable) {
+                onCriticalError.invoke(throwable)
+            }
+        }.join()
+    }
+}
+
+
+class TaskOneStageCodeBlockExecutor(
+
+) {
+
 }
