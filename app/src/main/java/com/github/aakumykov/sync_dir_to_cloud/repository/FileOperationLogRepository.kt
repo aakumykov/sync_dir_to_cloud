ca3302dc99804d697ba6ee8892080d4577038c7b
Andrey Kumykov <aakumykov@yandex.ru>
2026-01-25 10:09:35 +0300

    Класс FileOperationLogRepository

---
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/repository/FileOperationLogRepository.kt
@@ -0,0 +1,25 @@
+package com.github.aakumykov.sync_dir_to_cloud.repository
+
+import com.github.aakumykov.sync_dir_to_cloud.di.annotations.DispatcherIO
+import com.github.aakumykov.sync_dir_to_cloud.loggers2.entity.FileOperationLogItem
+import com.github.aakumykov.sync_dir_to_cloud.repository.room.dao.FileOperationLogDAO
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class FileOperationLogRepository @Inject constructor(
+    private val dao: FileOperationLogDAO,
+    @DispatcherIO private val dispatcher: CoroutineDispatcher,
+) {
+    suspend fun add(taskLogItem: FileOperationLogItem) = withContext(dispatcher) {
+        dao.add(taskLogItem)
+    }
+
+    suspend fun list(taskId: String, executionId: String): List<FileOperationLogItem> = withContext(dispatcher) {
+        dao.list(taskId, executionId)
+    }
+
+    suspend fun deleteAllFor(taskId: String) = withContext(dispatcher) {
+        dao.deleteAllForTask(taskId)
+    }
+}
