2c2cc5434ab0e2f606f9fd8454bc3e3de60a7909
aakumykov <aakumykov@yandex.ru>
2026-01-28 18:40:52 +0300

    Делаю AbstractInstructionsProcessor и CopyInstructionsProcessor.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/file_instructions_processor/AbstractInstructionsProcessor.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/file_instructions_processor/AbstractInstructionsProcessor.kt
@@ -3,7 +3,6 @@ package com.github.aakumykov.sync_dir_to_cloud.file_instructions_processor
 import com.github.aakumykov.sync_dir_to_cloud.job_holdes.OperationJobsHolder
 import com.github.aakumykov.sync_dir_to_cloud.loggers2.file_operation_logger_2.FileOperationLogger2
 import com.github.aakumykov.sync_dir_to_cloud.newRandomId
-import com.github.aakumykov.sync_dir_to_cloud.utils.launchWithStartCallback
 import kotlinx.coroutines.CoroutineScope
 
 abstract class AbstractInstructionsProcessor(
@@ -12,18 +11,28 @@ abstract class AbstractInstructionsProcessor(
 ) {
     fun process(scope: CoroutineScope, block: () -> Unit) {
         val jobId = newRandomId
-        scope.launchWithStartCallback(
+
+        runInCoroutineExtended(
+            scope = scope,
             onStart = { job ->
                 operationJobsHolder.addJob(jobId, job)
-            }
+                fileOperationLogger2.logStarted(jobId, operationStartsMessageId, operationDescription)
+            },
+            onFinish = {
+                fileOperationLogger2.logFinished(operationStartsMessageId, operationDescription)
+            },
+            onCancel = { e ->
+                operationJobsHolder.removeJob(jobId)
+            },
+            onError = { t ->
+                fileOperationLogger2.logError(operationStartsMessageId, operationDescription, t)
+            },
         ) {
-            fileOperationLogger2.logStarted(jobId, startMessageId, description)
             block.invoke()
-            fileOperationLogger2.logFinished(startMessageId, description)
         }
     }
 
-    abstract val startMessageId: Int
-    abstract val finishMessageId: Int
-    abstract val description: String
+    abstract val operationStartsMessageId: Int
+    abstract val operationFinishesMessageId: Int
+    abstract val operationDescription: String
 }
--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/file_instructions_processor/CopyInstructionsProcessor.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/file_instructions_processor/CopyInstructionsProcessor.kt
@@ -2,32 +2,35 @@ package com.github.aakumykov.sync_dir_to_cloud.file_instructions_processor
 
 import com.github.aakumykov.sync_dir_to_cloud.QUALIFIER_EXECUTION_ID
 import com.github.aakumykov.sync_dir_to_cloud.QUALIFIER_TASK_ID
+import com.github.aakumykov.sync_dir_to_cloud.aa_v5.level_40_sync_object.SyncObjectCopier
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.SyncInstruction
+import com.github.aakumykov.sync_dir_to_cloud.interfaces.for_repository.sync_object.SyncObjectDBReader
 import com.github.aakumykov.sync_dir_to_cloud.job_holdes.OperationJobsHolder
 import com.github.aakumykov.sync_dir_to_cloud.loggers2.file_operation_logger_2.FileOperationLogger2
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.CancellationException
 
 class CopyInstructionsProcessor @AssistedInject constructor(
     @Assisted(QUALIFIER_TASK_ID) private val taskId: String,
     @Assisted(QUALIFIER_EXECUTION_ID) private val executionId: String,
-    private final val fileOperationLogger2: FileOperationLogger2,
-    private final val operationJobsHolder: OperationJobsHolder
+    private val syncObjectDBReader: SyncObjectDBReader,
+    private val syncObjectCopier: SyncObjectCopier,
 )
     : AbstractInstructionsProcessor(fileOperationLogger2, operationJobsHolder)
 {
     fun process(instruction: SyncInstruction) {
-        TODO("Not yet implemented")
+        val syncObject = syncObjectDBReader.getSyncObject()
     }
 
-    override val startMessageId: Int
+    override val operationStartsMessageId: Int
         get() = TODO("Not yet implemented")
 
-    override val finishMessageId: Int
+    override val operationFinishesMessageId: Int
         get() = TODO("Not yet implemented")
 
-    override val description: String
+    override val operationDescription: String
         get() = TODO("Not yet implemented")
 }
 
