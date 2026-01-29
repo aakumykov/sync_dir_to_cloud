5a44409ddc6fe7a2fd49f6efb09abdb091adbd88
Andrey Kumykov <aakumykov@yandex.ru>
2026-01-29 23:00:42 +0300

    Reapply "Переделываю."
    This reverts commit 079228c75bf027ebf39e30b6146852c8bb9834a1.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/file_instructions_processor/InstructionsProcessor.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/file_instructions_processor/InstructionsProcessor.kt
@@ -1,11 +1,13 @@
 package com.github.aakumykov.sync_dir_to_cloud.file_instructions_processor
 
+import androidx.annotation.StringRes
 import kotlinx.coroutines.CoroutineScope
 
 interface InstructionsProcessor {
     suspend fun process(
         scope: CoroutineScope,
-        logMessageSupplier: FileOperationLogMessageSupplier,
+        @StringRes operationNameId: Int,
+        processedObjectRelativePath: String,
         codeBlock: suspend () -> Unit
     )
 }
