051d9bf4378f5280e33811bc2f1b067732833d46
aakumykov <aakumykov@yandex.ru>
2026-02-10 14:44:24 +0300

    Активировал SQL-представление. Какая-то проблема.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/sync_log/model/SyncLogItem.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/sync_log/model/SyncLogItem.kt
@@ -1,24 +1,37 @@
 package com.github.aakumykov.sync_dir_to_cloud.view.sync_log.model
 
+import androidx.room.ColumnInfo
 import androidx.room.DatabaseView
 import com.github.aakumykov.sync_dir_to_cloud.enums.LogItemType
 
-/*@DatabaseView(
+@DatabaseView(
     viewName = "sync_log",
-    value = "SELECT id as origLogId, task_id, execution_id, message, "
-)*/
+    value = "SELECT id as orig_log_id, task_id, execution_id, message as text FROM task_logs " +
+            "UNION ALL SELECT id as orig_log_id, task_id, execution_id, message as text FROM instruction_logs " +
+            "UNION ALL SELECT id as orig_log_id, task_id, execution_id, message as text FROM file_operation_logs"
+)
 data class SyncLogItem(
+    @ColumnInfo(name = "orig_log_id")
     val origLogId: String,
+
     val timestamp: Long,
+
+    @ColumnInfo(name = "log_item_type")
     val logItemType: LogItemType,
+
+    @ColumnInfo(name = "task_id")
     val taskId: String,
+
+    @ColumnInfo(name = "execution_id")
     val executionId: String,
+
     val text: String?,
 ) {
     val key: String get() = "${taskId}${executionId}${timestamp}"
 
-
     override fun toString(): String {
         return "SyncLogItem(origLogId='$origLogId', timestamp=$timestamp, logItemType=$logItemType, taskId='$taskId', executionId='$executionId', text=$text, key='$key')"
     }
+
+
 }
