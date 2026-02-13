b1fec55c0778f8dd50b4e1163c6054f8e229bac0
aakumykov <aakumykov@yandex.ru>
2026-02-13 16:10:10 +0300

    Выделил список элементов в журнале синхронизации в отдельную функцию.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/sync_log_compose/SyncLog.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/sync_log_compose/SyncLog.kt
@@ -8,12 +8,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -35,48 +39,81 @@ fun SyncLog(modifier: Modifier = Modifier,
         viewModel.startWorking(taskId, executionId)
     }
 
-    val listState = viewModel.logOfSyncListFlow
-        .collectAsState(emptyList())
+    val listState = viewModel.logOfSyncListFlow.collectAsState(emptyList())
 
+    ListWithoutScroll(listState, modifier = modifier)
+}
+
+
+@Composable
+fun ListWithoutScroll(listState: State<List<LogOfSync>>, modifier: Modifier = Modifier) {
     LazyColumn(modifier = modifier.fillMaxSize()) {
         items(items = listState.value, key = { it.key }) { logOfSync: LogOfSync ->
-            Column (
+            LogListItem(logOfSync)
+        }
+    }
+}
+
+
+@Composable
+fun AutoScrollingLazyColumn(listState: State<List<LogOfSync>>, modifier: Modifier = Modifier) {
+
+    val lazyListState = rememberLazyListState()
+
+    LaunchedEffect(listState.value.size) {
+        if (listState.value.isNotEmpty()) {
+            lazyListState.animateScrollToItem(listState.value.size - 1)
+        }
+    }
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        state = lazyListState
+    ) {
+        items(listState.value, key = { it.key }) { logOfSync: LogOfSync ->
+            LogListItem(logOfSync)
+        }
+    }
+}
+
+
+@Composable
+fun LogListItem(logOfSync: LogOfSync, modifier: Modifier = Modifier) {
+    Column (
+        modifier = modifier
+            .padding(4.dp)
+            .background(Color(0xffeeeeee), shape = RoundedCornerShape(8.dp))
+            .padding(horizontal = 12.dp, vertical = 6.dp)
+            .fillMaxWidth()
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                painter = painterResource(
+                    when(logOfSync.logItemType) {
+                        LogItemType.BUSY -> R.drawable.ic_sync_log_busy
+                        LogItemType.SUCCESS -> R.drawable.ic_sync_log_success
+                        LogItemType.ERROR -> R.drawable.ic_sync_log_error
+                        LogItemType.CANCELLED -> R.drawable.ic_sync_log_cancelled
+                    }
+                ),
+                contentDescription = null,
+                modifier = Modifier.padding(end = 10.dp),
+                tint = Color(0xFFB9B9B9)
+            )
+            Text(
+                text = logOfSync.text ?: "",
+                fontSize = 16.sp,
+            )
+        }
+        if (null != logOfSync.subText) {
+            Text(
+                text = logOfSync.subText,
+                fontSize = 13.sp,
                 modifier = Modifier
-                    .padding(4.dp)
-                    .background(Color(0xffeeeeee), shape = RoundedCornerShape(8.dp))
-                    .padding(horizontal = 12.dp, vertical = 6.dp)
-                    .fillMaxWidth()
-            ) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Icon(
-                        painter = painterResource(
-                            when(logOfSync.logItemType) {
-                                LogItemType.BUSY -> R.drawable.ic_sync_log_busy
-                                LogItemType.SUCCESS -> R.drawable.ic_sync_log_success
-                                LogItemType.ERROR -> R.drawable.ic_sync_log_error
-                                LogItemType.CANCELLED -> R.drawable.ic_sync_log_cancelled
-                            }
-                        ),
-                        contentDescription = null,
-                        modifier = Modifier.padding(end = 10.dp),
-                        tint = Color(0xFFB9B9B9)
-                    )
-                    Text(
-                        text = logOfSync.text ?: "",
-                        fontSize = 16.sp,
-                    )
-                }
-                if (null != logOfSync.subText) {
-                    Text(
-                        text = logOfSync.subText,
-                        fontSize = 13.sp,
-                        modifier = Modifier
-                            .padding(top = 5.dp)
-                            .background(Color(0xFFFCFCFC), shape = RoundedCornerShape(4.dp))
-                            .padding(horizontal = 6.dp)
-                    )
-                }
-            }
+                    .padding(top = 5.dp)
+                    .background(Color(0xFFFCFCFC), shape = RoundedCornerShape(4.dp))
+                    .padding(horizontal = 6.dp)
+            )
         }
     }
 }
