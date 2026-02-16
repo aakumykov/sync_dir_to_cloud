a6a7bb880d4677e36d4dbdaf80513933bf5a71f2
Andrey Kumykov <aakumykov@yandex.ru>
2026-02-16 14:40:06 +0300

    Пометил SyncTaskLogDAO к удалению.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/repository/room/dao/SyncTaskLogDAO.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/repository/room/dao/SyncTaskLogDAO.kt
@@ -2,44 +2,13 @@ package com.github.aakumykov.sync_dir_to_cloud.repository.room.dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
-import androidx.room.Insert
 import androidx.room.Query
 import com.github.aakumykov.sync_dir_to_cloud.domain.entities.TaskLogEntry
 
+@Deprecated("к удалению")
 @Dao
 interface SyncTaskLogDAO {
 
-    @Insert
-    suspend fun addTaskLog(taskLogEntry: TaskLogEntry)
-
-    @Query("DELETE FROM ${TaskLogEntry.TABLE_NAME} WHERE task_id = :taskId")
-    suspend fun deleteEntriesForTask(taskId: String)
-
-    @Query("SELECT * FROM ${TaskLogEntry.TABLE_NAME} WHERE task_id = :taskId")
-    suspend fun listLogsForTask(taskId: String): List<TaskLogEntry>
-
     @Query("SELECT * FROM ${TaskLogEntry.TABLE_NAME} WHERE task_id = :taskId ORDER BY start_time DESC")
     fun getLogsForTask(taskId: String): LiveData<List<TaskLogEntry>>
-
-
-    @Query("UPDATE ${TaskLogEntry.TABLE_NAME} " +
-            "SET " +
-            "finish_time = :finishTime, " +
-            "entry_type = 'FINISH' " +
-            "WHERE " +
-            "task_id = :taskId AND " +
-            "execution_id = :executionId")
-    suspend fun updateAsSuccess(taskId: String, executionId: String, finishTime: Long)
-
-
-    @Query("UPDATE ${TaskLogEntry.TABLE_NAME} " +
-            "SET " +
-            "finish_time = :finishTime, " +
-            "entry_type = 'ERROR', " +
-            "error_msg = :errorMsg " +
-            "WHERE " +
-            "task_id = :taskId AND " +
-            "execution_id = :executionId")
-    suspend fun updateAsError(taskId: String, executionId: String, finishTime: Long, errorMsg: String?)
-
 }
