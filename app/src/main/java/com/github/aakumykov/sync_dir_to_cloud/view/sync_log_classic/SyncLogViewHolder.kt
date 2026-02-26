b8317d0f54d4f9efa51a1e28eeaedb60e048d29a
Andrey Kumykov <aakumykov@yandex.ru>
2026-02-26 07:46:59 +0300

    Добавил в журнал синхронизации иконки состояния.

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/sync_log_classic/SyncLogViewHolder.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/view/sync_log_classic/SyncLogViewHolder.kt
@@ -1,10 +1,12 @@
 package com.github.aakumykov.sync_dir_to_cloud.view.sync_log_classic
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.github.aakumykov.sync_dir_to_cloud.R
+import com.github.aakumykov.sync_dir_to_cloud.enums.LogItemType
 import com.github.aakumykov.sync_dir_to_cloud.extensions.toPercentOf100
 import com.github.aakumykov.sync_dir_to_cloud.view.sync_log.model.LogOfSync
 import com.github.aakumykov.sync_dir_to_cloud.view.sync_log.model.isActiveFileOperation
@@ -16,6 +18,15 @@ class SyncLogViewHolder(
 
     fun init(item: LogOfSync) {
         itemView.apply {
+            findViewById<ImageView>(R.id.log_of_sync_status_icon).setImageResource(
+                when(item.logItemType) {
+                    LogItemType.BUSY -> R.drawable.ic_sync_log_busy
+                    LogItemType.SUCCESS -> R.drawable.ic_sync_log_success
+                    LogItemType.CANCELLED -> R.drawable.ic_sync_log_cancelled
+                    LogItemType.ERROR -> R.drawable.ic_sync_log_error
+                }
+            )
+
             findViewById<TextView>(R.id.log_of_sync_text).text = item.text
 
             findViewById<TextView>(R.id.log_of_sync_sub_text).apply {
