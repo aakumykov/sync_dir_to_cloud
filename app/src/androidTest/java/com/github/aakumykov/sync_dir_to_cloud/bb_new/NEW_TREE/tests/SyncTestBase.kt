485cd0b7f5125a20e9ef9f91d3c9caab5011aa10
aakumykov <aakumykov@yandex.ru>
2026-08-25 17:27:17 +0300

    Сомнительная попытка решить проблему с тестом добавлением новых методов.

--- app/src/androidTest/java/com/github/aakumykov/sync_dir_to_cloud/bb_new/NEW_TREE/tests/SyncTestBase.kt
+++ app/src/androidTest/java/com/github/aakumykov/sync_dir_to_cloud/bb_new/NEW_TREE/tests/SyncTestBase.kt
@@ -142,6 +142,9 @@ abstract class SyncTestBase : StorageAccessTestCase() {
     protected val sDeepDirName = randomDeepDirName
     protected val tDeepDirName = randomDeepDirName
 
+    protected fun sDeepDirName(depth: Int): String = randomDeepDirName(depth, depth)
+    protected fun tDeepDirName(depth: Int): String = randomDeepDirName(depth, depth)
+
     protected val sDeepDir = fileHelper.dirInSource(sDeepDirName)
     protected val tDeepDir = fileHelper.dirInTarget(tDeepDirName)
 
