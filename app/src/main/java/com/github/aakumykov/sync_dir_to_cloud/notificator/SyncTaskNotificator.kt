a39c00559880e237909a7519f5a34d0d15726689
aakumykov <aakumykov@yandex.ru>
2026-08-26 13:18:52 +0300

    Применил к уведомлению о прогрессе стиль "большой текст".

--- app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/notificator/SyncTaskNotificator.kt
+++ app/src/main/java/com/github/aakumykov/sync_dir_to_cloud/notificator/SyncTaskNotificator.kt
@@ -129,6 +129,7 @@ class SyncTaskNotificator @AssistedInject constructor(
         NotificationCompat.Builder(appContext, notificationChannelConfig.progress.channelId)
             .setContentTitle(getString(R.string.sync_task_progress_notification_title))
             .setSmallIcon(R.drawable.ic_sync_task_notification_progress)
+            .setStyle(NotificationCompat.BigTextStyle())
             .setAutoCancel(false)
             .setOngoing(true)
             .setOnlyAlertOnce(true)
